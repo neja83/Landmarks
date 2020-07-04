53c2dc3ae15f7c8ab096b57862fdf48f85afaede
Eugene Krapivenko <ekrapivenko@gmail.com>
2020-07-04 23:41:53 +0300

    Initial Commit

---
+++ Landmarks/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by Eugene Krapivenko on 04.07.2020.
+//  Copyright © 2020 Eugene Krapivenko. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
