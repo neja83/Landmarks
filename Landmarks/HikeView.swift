60c6254607ccee2ef26f4a0da6cf882deceec708
Eugene Krapivenko <ekrapivenko@gmail.com>
2020-07-07 22:45:19 +0300

    add start views and data for lesson

---
+++ Landmarks/HikeView.swift
@@ -0,0 +1,54 @@
+//
+//  HikeView.swift
+//  Landmarks
+//
+//  Created by Eugene Krapivenko on 07.07.2020.
+//  Copyright © 2020 Eugene Krapivenko. All rights reserved.
+//
+
+import SwiftUI
+
+struct HikeView: View {
+    var hike: Hike
+    @State private var showDetail = false
+
+    var body: some View {
+        VStack {
+            HStack {
+                HikeGraph(hike: hike, path: \.elevation)
+                    .frame(width: 50, height: 30)
+
+                VStack(alignment: .leading) {
+                    Text(hike.name)
+                        .font(.headline)
+                    Text(hike.distanceText)
+                }
+
+                Spacer()
+
+                Button(action: {
+                    self.showDetail.toggle()
+                }) {
+                    Image(systemName: "chevron.right.circle")
+                        .imageScale(.large)
+                        .rotationEffect(.degrees(showDetail ? 90 : 0))
+                        .padding()
+                        .animation(.easeInOut)
+                }
+            }
+
+            if showDetail {
+                HikeDetail(hike: hike)
+            }
+        }
+    }
+}
+
+struct HikeView_Previews: PreviewProvider {
+    static var previews: some View {
+        VStack {
+            HikeView(hike: hikeData[0]).padding()
+            Spacer()
+        }
+    }
+}
---
+++ Landmarks/Structs/Hiko.swift
@@ -0,0 +1,32 @@
+//
+//  Hiko.swift
+//  Landmarks
+//
+//  Created by Eugene Krapivenko on 07.07.2020.
+//  Copyright © 2020 Eugene Krapivenko. All rights reserved.
+//
+
+import Foundation
+
+struct Hike: Codable, Hashable, Identifiable {
+    var name: String
+    var id: Int
+    var distance: Double
+    var difficulty: Int
+    var observations: [Observation]
+
+    static var formatter = LengthFormatter()
+    
+    var distanceText: String {
+        return Hike.formatter
+            .string(fromValue: distance, unit: .kilometer)
+    }
+
+    struct Observation: Codable, Hashable {
+        var distanceFromStart: Double
+        
+        var elevation: Range<Double>
+        var pace: Range<Double>
+        var heartRate: Range<Double>
+    }
+}
